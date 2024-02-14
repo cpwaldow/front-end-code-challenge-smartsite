b8cdfe901f338d4a2d2b48c4c7b30e39f521a380
Carlos Eduardo <cpwaldow@gmail.com>
2024-02-14 13:35:57 -0300

    deletando App.test.tsx

--- src/tests/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-it('should show "REABERTURA SMART FIT" text', () => {
-  render(<App />);
-  expect(screen.getByText('REABERTURA SMART FIT')).toBeInTheDocument();
-});
