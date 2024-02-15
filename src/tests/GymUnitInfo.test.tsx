3beb3f92b580d0fe238b65a62f654ed7e2a8d88f
Carlos Eduardo <cpwaldow@gmail.com>
2024-02-14 21:59:26 -0300

    adicionando testes para o src das imagens do componente GymUnitInfo

--- src/tests/GymUnitInfo.test.tsx
+++ src/tests/GymUnitInfo.test.tsx
@@ -42,19 +42,34 @@ const icons = [
   'lockerroom - allowed',
 ];
 
+const iconsSrc = [
+  '/src/components/assets/required-mask.png',
+  '/src/components/assets/required-towel.png',
+  '/src/components/assets/partial-fountain.png',
+  '/src/components/assets/required-lockerroom.png',
+];
+
 describe('GymUnitInfo component test gym', () => {
   it('Render the correct info about Gym open', () => {
     render(<GymUnitInfo info={gymInfos[0]} />);
     const isOpenOrIsClose = screen.getByTestId('isOpen-or-isClose');
     expect(isOpenOrIsClose).toHaveTextContent('Aberto');
+
     expect(screen.getByText('Dom Severino')).toBeInTheDocument();
     expect(
       screen.getByText('Av. Dom Severino, 1733 - Fátima'),
     ).toBeInTheDocument();
     expect(screen.getByText('Teresina, PI')).toBeInTheDocument();
-    icons.forEach((item) => {
-      expect(screen.getByAltText(item)).toBeInTheDocument();
+
+    icons.forEach((item, index) => {
+      const image = screen.getByAltText(item);
+      expect(image).toBeInTheDocument();
+      expect(image).toHaveProperty(
+        'src',
+        `http://localhost:3000${iconsSrc[index]}`,
+      );
     });
+
     expect(screen.getByText('Seg. à Sex.')).toBeInTheDocument();
     expect(screen.getByText('06h às 22h')).toBeInTheDocument();
     expect(screen.getByText('Sáb.')).toBeInTheDocument();
@@ -65,13 +80,16 @@ describe('GymUnitInfo component test gym', () => {
 
   it('Render the correct info about Gym close', () => {
     render(<GymUnitInfo info={gymInfos[1]} />);
+
     const isOpenOrIsClose = screen.getByTestId('isOpen-or-isClose');
     expect(isOpenOrIsClose).toHaveTextContent('Fechado');
+
     expect(screen.getByText('Presidente Prudente')).toBeInTheDocument();
     expect(
       screen.getByText('Rua Siqueira Campos, 1545 - Vila Roberto'),
     ).toBeInTheDocument();
     expect(screen.getByText('Presidente Prudente, SP')).toBeInTheDocument();
+
     expect(() => screen.getByText('Seg. à Sex.')).toThrow();
     icons.forEach((item) => {
       expect(() => screen.getByAltText(item)).toThrow();
