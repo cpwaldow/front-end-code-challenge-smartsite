5194e32f73abe98637e2e53fda29ccbbad9b64a2
Carlos Eduardo <cpwaldow@gmail.com>
2024-02-21 22:26:15 -0300

    criando arquivo de testes para botao de encontrar unidades

---
+++ src/tests/BtnFindUnit.test.tsx
@@ -0,0 +1,7 @@
+import { screen, render } from '@testing-library/react';
+import BtnFindUnit from '../components/Form/BtnFindUnit';
+
+describe('Suite de testes com o componente BtnFindUnit', () => {
+  // o botão só é renderizado após mudar o estado global de gyms, pois retorna null
+  test.todo('Encontre o botão "ENCONTRAR UNIDADE" no documento');
+});
