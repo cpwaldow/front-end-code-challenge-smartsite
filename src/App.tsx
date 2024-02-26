40555738e295f4763149e11212423b0c37d74f1c
Carlos Eduardo <cpwaldow@gmail.com>
2024-02-26 10:32:15 -0300

    comentando geolocation e criando mock da API

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
     return position;
   };
-  getLocation();
+  // getLocation();
 
   return (
     <>
---
+++ src/tests/mocks/mockGyms.ts
@@ -0,0 +1,55 @@
+export const gyms = {
+  current_country_id: 1,
+  success: true,
+  total: 167,
+  wp_total: 116,
+  locations: [
+    {
+      id: 10998878976097,
+      title: 'Dom Severino',
+      content:
+        '\n<p>Av. Dom Severino, 1733 &#8211; Fátima<br>Teresina, PI</p>\n',
+      opened: true,
+      mask: 'required',
+      towel: 'required',
+      fountain: 'partial',
+      locker_room: 'allowed',
+      schedules: [
+        { weekdays: 'Seg. à Sex.', hour: '06h às 22h' },
+        { weekdays: 'Sáb.', hour: 'Fechada' },
+        { weekdays: 'Dom.', hour: 'Fechada' },
+      ],
+    },
+    {
+      id: 10998878976096,
+      title: 'Teresina Shopping',
+      content: '\n<p>Av. Raul Lopes, 1000 &#8211; Noivos<br>Teresina, PI</p>\n',
+      opened: true,
+      mask: 'required',
+      towel: 'required',
+      fountain: 'partial',
+      locker_room: 'allowed',
+      schedules: [
+        { weekdays: 'Seg. à Sex.', hour: '06h às 22h' },
+        { weekdays: 'Sáb.', hour: 'Fechada' },
+        { weekdays: 'Dom.', hour: 'Fechada' },
+      ],
+    },
+    {
+      id: 10998878976088,
+      title: 'Parnamirim Centro',
+      content:
+        '\n<p>Avenida Brigadeiro Everaldo Breves, 780 &#8211; Centro<br>Parnamirim, RN</p>\n',
+      opened: true,
+      mask: 'required',
+      towel: 'required',
+      fountain: 'partial',
+      locker_room: 'allowed',
+      schedules: [
+        { weekdays: 'Seg. à Sex.', hour: '06h às 22h' },
+        { weekdays: 'Sáb.', hour: '09h às 18h' },
+        { weekdays: 'Dom.', hour: 'Fechada' },
+      ],
+    },
+  ],
+};
